320534c9e7ff817e26c0dc3cc96c314a5ec86406
Tha Jester <hans@jestersoft.nl>
2018-03-04 18:24:08 +0100

    Initial version

---
+++ app/src/main/java/nl/jestersoft/kotlinapp/RandomActivity.kt
@@ -0,0 +1,33 @@
+package nl.jestersoft.kotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_random.*
+import java.util.*
+
+class RandomActivity : AppCompatActivity() {
+
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random)
+        showRandomNumber()
+    }
+
+    fun showRandomNumber(){
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+
+        val random = Random()
+        var randomInt = 0;
+        if (count > 0) {
+            randomInt = random.nextInt(count+1)
+        }
+
+        textview_random.text = Integer.toString(randomInt)
+
+        textview_label.text = getString(R.string.random_heading, count)
+    }
+}
